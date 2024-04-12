847bff806a78786198376c129145160c1a8b685c
PR3D4T0R15 <mati.m@opoczta.pl>
2024-04-12 19:09:22 +0200

    Dodaj pliki projektów.

---
+++ PO_KolkoKrzyzyk_Client/PO_KolkoKrzyzyk_Client.cpp
@@ -0,0 +1,12 @@
+﻿// PO_KolkoKrzyzyk_Client.cpp: definiuje punkt wejścia dla aplikacji.
+//
+
+#include "PO_KolkoKrzyzyk_Client.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
