670f7d3f5f0b4fcfe5d2b81570c2567008339e00
PR3D4T0R15 <mati.m@opoczta.pl>
2024-04-22 10:20:41 +0200

    add cmake project structure

---
+++ PO_KolkoKrzyzyk_Client/src/main.cpp
@@ -0,0 +1,10 @@
+#include "include/main.h"
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+
+int main(int argc, char* argv[])
+{
+	QGuiApplication app(argc, argv);
+	QQmlApplicationEngine engine;
+}
