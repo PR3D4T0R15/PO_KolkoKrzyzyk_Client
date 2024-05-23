ec39bf079103d25437db0cc32805de7f569ce53c
JuliaCz18 <julkacz@onet.pl>
2024-05-24 00:46:46 +0200

    window control class update

--- PO_KolkoKrzyzyk_Client/include/WindowControl.h
+++ PO_KolkoKrzyzyk_Client/include/WindowControl.h
@@ -3,6 +3,7 @@
 #include <QObject>
 #include <QDebug>
 #include <QQuickItem>
+#include <QMetaObject>
 
 class WindowControl : public QObject
 {
@@ -13,11 +14,36 @@ public:
 	~WindowControl();
 
 public slots:
+	//QML
 	void uiButtonClicked(QObject* obj);
 
+	//C++
+	void goToStartView();
+	void goToLoginView();
+	void goToNewAccountView();
+	void goToHomeView();
+	void goToMatchmakingView();
+	void goToGameView();
+	void goToEndgameView();
+
 signals:
+	//QML
 	void uiChangeView(QString view);
 	void uiChangeState(QString state);
+
+	//C++
+	void welcomeButtonClicked();
+	void newAccountButtonClicked();
+	void logInButtonClicked();
+	void createNewAccountButtonClicked();
+	void goToLoginButtonClicked();
+	void playButtonClicked();
+	void logOutButtonClicked();
+	void exitButtonClicked();
+	void exitMatchmakingButtonClicked();
+	void gameLeaveButtonClicked();
+	void playAgainButtonClicked();
+	void goToMenuButtonClicked();
 
 private:
 };
--- PO_KolkoKrzyzyk_Client/src/WindowControl.cpp
+++ PO_KolkoKrzyzyk_Client/src/WindowControl.cpp
@@ -13,4 +13,8 @@ void WindowControl::uiButtonClicked(QObject* obj)
 	QString cppId = obj->property("cppId").toString();
 
 	qDebug() << "Button" << cppId;
+
+	cppId = cppId + "Clicked";
+
+	QMetaObject::invokeMethod(this, cppId.toUtf8().constData(), Qt::DirectConnection);
 }
