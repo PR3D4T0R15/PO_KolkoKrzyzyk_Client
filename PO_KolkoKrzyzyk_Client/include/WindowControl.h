27fca177568d47e02bcda4bbfec65ee51bf570ac
JuliaCz18 <julkacz@onet.pl>
2024-05-29 15:10:15 +0200

    ui control class changes

--- PO_KolkoKrzyzyk_Client/include/WindowControl.h
+++ PO_KolkoKrzyzyk_Client/include/WindowControl.h
@@ -3,7 +3,10 @@
 #include <QObject>
 #include <QDebug>
 #include <QQuickItem>
-#include <QMetaObject>
+#include <QStateMachine>
+#include <QState>
+#include <QFinalState>
+#include <QStringList>
 
 class WindowControl : public QObject
 {
@@ -19,19 +22,92 @@ public:
 	QString getErrInfo();
 
 public slots:
-	//QML
-	void uiButtonClicked(QObject* obj);		//message from QML
 
-	
+private slots:
+	void stateInfo();
 
 signals:
-	//QML
-	void uiChangeView(QString view);		//message to QML
-	void uiChangeState(QString state);		//message to QML
-
 	//Q_PROPERTY
 	void errInfoChanged();
 
+	//transitions buttons
+	void welcomeButtonClicked();
+	void newAccountButtonClicked();
+	void createNewAccountButtonClicked();
+	void goToLoginButtonClicked();
+	void logInButtonClicked();
+	void logOutButtonClicked();
+	void exitButtonClicked();
+	void playButtonClicked();
+	void gameLeaveButtonClicked();
+	void playAgainButtonClicked();
+	void goToMenuButtonClicked();
+
+	void aboutButtonClicked();
+	void settingsButtonClicked();
+	void soundButtonClicked();
+
+	void connectionOk();
+	void connectionNotOk();
+	void accountCreated();
+	void accountNotCreated();
+	void loginOk();
+	void loginNotOk();
+	void logoutOk();
+	void matchmakingNotOk();
+	void matchmakingOk();
+	void exitMatchmakingOk();
+	void exitMatchmakingNotOk();
+
+	
+	void exitMatchmaking();
+	void gameReady();
+	void gameStarted();
+	void gameEnd();
+	
+
+	//state entered signals
+	void inStartView();
+	void inConnectedToServer();
+	void inLoginView();
+	void inLogin();
+	void inLogout();
+	void inNewAccountView();
+	void inCreateAccount();
+	void inHomeView();
+	void inStartMatchmaking();
+	void inStopMatchmaking();
+	void inMatchmakingView();
+	void inStartGame();
+	void inEndGame();
+	void inGameView();
+	void inEndGameView();
+	void inExit();
+
+
 private:
+	//Q_PROPERTY
 	QString _errInfo;
+
+	//fsm
+	QStateMachine* _windowView;
+	QState* _startView;
+	QState* _connectToServer;
+	QState* _loginView;
+	QState* _login;
+	QState* _logout;
+	QState* _newAccountView;
+	QState* _createAccount;
+	QState* _homeView;
+	QState* _startMatchmaking;
+	QState* _stopMatchmaking;
+	QState* _matchmakingView;
+	QState* _startGame;
+	QState* _endGame;
+	QState* _gameView;
+	QState* _endgameView;
+	QFinalState* _exit;
+
+	//lists
+	QStringList _buttons;
 };
--- PO_KolkoKrzyzyk_Client/src/WindowControl.cpp
+++ PO_KolkoKrzyzyk_Client/src/WindowControl.cpp
@@ -1,12 +1,162 @@
 #include "include/WindowControl.h"
 
+#include "include/WindowViews.h"
+
 WindowControl::WindowControl(QObject* parent) : QObject(parent)
 {
 	_errInfo = "";
+
+	//state machine object
+	_windowView = new QStateMachine();
+
+	//state objects
+	_startView = new QState();
+	_connectToServer = new QState();
+	_loginView = new QState();
+	_login = new QState();
+	_logout = new QState();
+	_newAccountView = new QState();
+	_createAccount = new QState();
+	_homeView = new QState();
+	_startMatchmaking = new QState();
+	_stopMatchmaking = new QState();
+	_matchmakingView = new QState();
+	_startGame = new QState();
+	_endGame = new QState();
+	_gameView = new QState();
+	_endgameView = new QState();
+	_exit = new QFinalState();
+
+	//add names
+	_startView->setObjectName("startView");
+	_connectToServer->setObjectName("connectToServer");
+	_loginView->setObjectName("loginView");
+	_login->setObjectName("login");
+	_logout->setObjectName("logout");
+	_newAccountView->setObjectName("_newAccount");
+	_createAccount->setObjectName("createAccount");
+	_homeView->setObjectName("_homeView");
+	_startMatchmaking->setObjectName("startMatchmaking");
+	_stopMatchmaking->setObjectName("stopMatchmaking");
+	_matchmakingView->setObjectName("_matchmakingView");
+	_startGame->setObjectName("startGame");
+	_endGame->setObjectName("endGame");
+	_gameView->setObjectName("_gameView");
+	_endgameView->setObjectName("_endgameView");
+	_exit->setObjectName("_exit");
+
+	//adding states to machine
+	_windowView->addState(_startView);
+	_windowView->addState(_connectToServer);
+	_windowView->addState(_loginView);
+	_windowView->addState(_login);
+	_windowView->addState(_logout);
+	_windowView->addState(_newAccountView);
+	_windowView->addState(_createAccount);
+	_windowView->addState(_homeView);
+	_windowView->addState(_startMatchmaking);
+	_windowView->addState(_stopMatchmaking);
+	_windowView->addState(_matchmakingView);
+	_windowView->addState(_startGame);
+	_windowView->addState(_endGame);
+	_windowView->addState(_gameView);
+	_windowView->addState(_endgameView);
+	_windowView->addState(_exit);
+
+	//initial state
+	_windowView->setInitialState(_startView);
+
+	//transitions
+	_startView->addTransition(this, &WindowControl::welcomeButtonClicked, _connectToServer);			//button enter the game
+	_connectToServer->addTransition(this, &WindowControl::connectionOk, _loginView);					//signal connected to server
+	_connectToServer->addTransition(this, &WindowControl::connectionNotOk, _startView);				//signal connection failed
+	_loginView->addTransition(this, &WindowControl::newAccountButtonClicked, _newAccountView);		//button for new account
+	_newAccountView->addTransition(this, &WindowControl::goToLoginButtonClicked, _loginView);			//button go back to login view
+	_newAccountView->addTransition(this, &WindowControl::createNewAccountButtonClicked, _createAccount);			//button create new account
+	_createAccount->addTransition(this, &WindowControl::accountCreated, _loginView);					//signal account created
+	_createAccount->addTransition(this, &WindowControl::accountNotCreated, _newAccountView);			//signal account creation error
+	_loginView->addTransition(this, &WindowControl::logInButtonClicked, _login);						//button login
+	_login->addTransition(this, &WindowControl::loginOk, _homeView);									//signal login successful
+	_login->addTransition(this, &WindowControl::loginNotOk, _loginView);								//signal login failed
+	_homeView->addTransition(this, &WindowControl::logOutButtonClicked, _logout);						//button logout
+	_logout->addTransition(this, &WindowControl::logoutOk, _startView);								//signal logout successful
+	_homeView->addTransition(this, &WindowControl::exitButtonClicked, _exit);							//exit app
+	_homeView->addTransition(this, &WindowControl::playButtonClicked, _startMatchmaking);				//button  start matchmaking
+	_startMatchmaking->addTransition(this, &WindowControl::matchmakingNotOk, _homeView);				//signal error joining to matchmaking queue
+	_startMatchmaking->addTransition(this, &WindowControl::matchmakingOk, _matchmakingView);			//signal palyer in matchmaking queue
+	_matchmakingView->addTransition(this, &WindowControl::exitMatchmaking, _stopMatchmaking);			//button exit matchmaking queue
+	_stopMatchmaking->addTransition(this, &WindowControl::exitMatchmakingOk, _homeView);				//signal player removed from queue
+	_stopMatchmaking->addTransition(this, &WindowControl::exitMatchmakingNotOk, _matchmakingView);	//signal player not removed
+	_matchmakingView->addTransition(this, &WindowControl::gameReady, _startGame);						//signal game found
+	_startGame->addTransition(this, &WindowControl::gameStarted, _gameView);							//signal game started
+	_gameView->addTransition(this, &WindowControl::gameEnd, _endGame);								//signal game is finished
+	_gameView->addTransition(this, &WindowControl::gameLeaveButtonClicked, _endGame);					//button exit game before end
+	_endGame->addTransition(this, &WindowControl::playAgainButtonClicked, _startMatchmaking);			//button start another game
+	_endGame->addTransition(this, &WindowControl::goToMenuButtonClicked, _startMatchmaking);			//button go to menu
+
+
+	
+
+	//entered signals
+	QObject::connect(_startView, &QState::entered, this, &WindowControl::inStartView);
+	QObject::connect(_connectToServer, &QState::entered, this, &WindowControl::inConnectedToServer);
+	QObject::connect(_loginView, &QState::entered, this, &WindowControl::inLoginView);
+	QObject::connect(_login, &QState::entered, this, &WindowControl::inLogin);
+	QObject::connect(_logout, &QState::entered, this, &WindowControl::inLogout);
+	QObject::connect(_newAccountView, &QState::entered, this, &WindowControl::inNewAccountView);
+	QObject::connect(_createAccount, &QState::entered, this, &WindowControl::inCreateAccount);
+	QObject::connect(_homeView, &QState::entered, this, &WindowControl::inHomeView);
+	QObject::connect(_startMatchmaking, &QState::entered, this, &WindowControl::inStartMatchmaking);
+	QObject::connect(_stopMatchmaking, &QState::entered, this, &WindowControl::inStopMatchmaking);
+	QObject::connect(_matchmakingView, &QState::entered, this, &WindowControl::inMatchmakingView);
+	QObject::connect(_startGame, &QState::entered, this, &WindowControl::inStartGame);
+	QObject::connect(_endGame, &QState::entered, this, &WindowControl::inEndGame);
+	QObject::connect(_gameView, &QState::entered, this, &WindowControl::inGameView);
+	QObject::connect(_endgameView, &QState::entered, this, &WindowControl::inEndGameView);
+
+	//tmp signals
+	QObject::connect(_startView, &QState::entered, this, &WindowControl::stateInfo);
+	QObject::connect(_connectToServer, &QState::entered, this, &WindowControl::stateInfo);
+	QObject::connect(_loginView, &QState::entered, this, &WindowControl::stateInfo);
+	QObject::connect(_login, &QState::entered, this, &WindowControl::stateInfo);
+	QObject::connect(_logout, &QState::entered, this, &WindowControl::stateInfo);
+	QObject::connect(_newAccountView, &QState::entered, this, &WindowControl::stateInfo);
+	QObject::connect(_createAccount, &QState::entered, this, &WindowControl::stateInfo);
+	QObject::connect(_homeView, &QState::entered, this, &WindowControl::stateInfo);
+	QObject::connect(_startMatchmaking, &QState::entered, this, &WindowControl::stateInfo);
+	QObject::connect(_stopMatchmaking, &QState::entered, this, &WindowControl::stateInfo);
+	QObject::connect(_matchmakingView, &QState::entered, this, &WindowControl::stateInfo);
+	QObject::connect(_startGame, &QState::entered, this, &WindowControl::stateInfo);
+	QObject::connect(_endGame, &QState::entered, this, &WindowControl::stateInfo);
+	QObject::connect(_gameView, &QState::entered, this, &WindowControl::stateInfo);
+	QObject::connect(_endgameView, &QState::entered, this, &WindowControl::stateInfo);
+
+
+	QObject::connect(_windowView, &QState::finished, this, &WindowControl::inExit);
+
+	_windowView->start();
 }
 
 WindowControl::~WindowControl()
 {
+	_startView->deleteLater();
+	_connectToServer->deleteLater();
+	_loginView->deleteLater();
+	_login->deleteLater();
+	_logout->deleteLater();
+	_newAccountView->deleteLater();
+	_createAccount->deleteLater();
+	_homeView->deleteLater();
+	_startMatchmaking->deleteLater();
+	_stopMatchmaking->deleteLater();
+	_matchmakingView->deleteLater();
+	_startGame->deleteLater();
+	_endGame->deleteLater();
+	_gameView->deleteLater();
+	_endgameView->deleteLater();
+	_exit->deleteLater();
+
+	_windowView->deleteLater();
 }
 
 QString WindowControl::getErrInfo()
@@ -14,13 +164,17 @@ QString WindowControl::getErrInfo()
 	return _errInfo;
 }
 
-//get button id from QML and send with signal to other class in cpp
-void WindowControl::uiButtonClicked(QObject* obj)
+void WindowControl::stateInfo()
 {
-	QString cppId = obj->property("cppId").toString();
-
-	qDebug() << "Button" << cppId;
-	setErrInfo("test");
+	QState* state = qobject_cast<QState*>(sender());
+	QString stateName = state->objectName();
+	if (state) {
+		qDebug() << "Entered state:" << stateName;
+	}
+	if (stateName == "connectToServer")
+	{
+		emit connectionOk();
+	}
 }
 
 void WindowControl::setErrInfo(const QString& newErrInfo)
